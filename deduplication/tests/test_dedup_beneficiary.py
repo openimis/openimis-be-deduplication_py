6cd7e9d963d14bf2d2b9cc1f4d39b5a19dde9394
Damian Borowiecki <dborowiecki@soldevelo.com>
2024-10-22 14:52:56 +0200

    Disabled check for MSSQL and changed assertion

--- deduplication/tests/test_dedup_beneficiary.py
+++ deduplication/tests/test_dedup_beneficiary.py
@@ -1,16 +1,10 @@
-import unittest
-from decimal import Decimal
-
 from django.test import TestCase
-from django.db import transaction
-from django.db.models import Q
-from django.conf import settings
 from deduplication.services import get_beneficiary_duplication_aggregation
 from deduplication.tests.data.dedup_beneficiary import benefit_plan_data, individuals_data
 from deduplication.tests.helpers import LogInHelper
 from individual.models import Individual
 from social_protection.models import Beneficiary, BenefitPlan, BeneficiaryStatus
-
+from django.db import connection
 
 class DedupBeneficiaryTestCase(TestCase):
     user = None
@@ -42,7 +36,12 @@ class DedupBeneficiaryTestCase(TestCase):
             b.save(username=cls.user.username)
             cls.bens.append(b)
 
-    @unittest.skipIf(settings.DB_DEFAULT == 'mssql', "Skipping tests because Feature is not supported in MSSQL Engine.")
     def test_deduplication_aggregation(self):
-        res = get_beneficiary_duplication_aggregation(['individual__first_name', 'k1'], self.bp.id)
-        self.assertListEqual(list(res), [{'id_count': 2, 'individual__first_name': 'first name 1', 'k1': 'k1 v1'}])
+        if connection.vendor == 'microsoft':
+            self.skipTest("This test can only be executed for PSQL database")
+        else:
+            res = get_beneficiary_duplication_aggregation(['individual__first_name', 'k1'], self.bp.id)
+            response = list(res)[0]
+            self.assertEquals(response['id_count'], 2)
+            self.assertEquals(response['individual__first_name'], 'first name 1')
+            self.assertEquals(response['k1'], 'k1 v1')
